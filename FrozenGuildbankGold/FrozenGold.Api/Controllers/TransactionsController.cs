c6e1885b4e827ac0a55030d9b65403c560030bf0
Patrick Allwood <patchandthat@gmail.com>
2020-06-29 16:33:09 +0100

    Txn API

---
+++ FrozenGuildbankGold/FrozenGold.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FrozenGold.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IDataSource _dataSource;
+        private readonly ILogger<TransactionsController> _logger;
+
+        public TransactionsController(ILogger<TransactionsController> logger, IDataSource dataSource)
+        {
+            _dataSource = dataSource ?? throw new ArgumentNullException(nameof(dataSource));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public TransactionHistory Get()
+        {
+            try
+            {
+                return new TransactionHistory
+                {
+                    LastUpdated = _dataSource.GetLastUpdatedDate(),
+                    Transactions = _dataSource.GetTransactionHistory().ToArray()
+                };
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to provode transactions");
+                throw;
+            }
+        }
+    }
+}
---
+++ FrozenGuildbankGold/FrozenGold.Api/TransactionHistory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FrozenGold.Api
+{
+    public class TransactionHistory
+    {
+        public DateTimeOffset LastUpdated { get; set; }
+        public Transaction[] Transactions { get; set; }
+    }
+}
